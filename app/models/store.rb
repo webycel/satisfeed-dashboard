6d6635e9e0fd89b4f9a4b6455749533e1cbeb742
webycel <anthony.web91@gmail.com>
2015-11-20 12:07:31 +0000

    show today's and yesterday's experience

--- app/models/store.rb
+++ app/models/store.rb
@@ -10,7 +10,19 @@ class Store < ActiveRecord::Base
 
 	def self.get_by_experience experiences, rating
 		filtered_list = Array.new
-		experiences.select { |k,experience| experience["experience"] == rating  }
+		experiences.select { |key, experience| experience["experience"] == rating  }
+	end
+
+	def self.filter_by_date time, experiences
+		if time == "today"
+			experiences.select do |key, experience|
+				experience["time"].to_date.today?
+			end
+		elsif time == "yesterday"
+			experiences.select do |key, experience|
+				experience["time"].to_date.advance(:days => 1).today?
+			end
+		end
 	end
 
 end
