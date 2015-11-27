2605eb53aedfd7a677064cfe9f34edad70f10756
Margo Urey <margo@margonline.co.uk>
2015-11-27 12:08:33 +0000

    fix filter menu

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,25 +1,21 @@
 class StoresController < ApplicationController
 
 	def index
-
 	end
 
 	def search
-		@filter = params["filter"]
 		@store = Store.find(params["store_id"])
-
 		if @store == "error"
 			redirect_to root_path, flash: {error: 'Sorry, couldn\'t find any data with this Store name.'}
 		else
 			redirect_to store_path(@store.name)
 		end
-		
 	end
 
 	def show
 		@store = Store.find(params[:id])
-		@filter_quality = params[:quality]
-		@filter_range = params[:range]
+		@filter_quality = params[:quality] || "all"
+		@filter_range = params[:range] || "anytime"
 		@experiences = @store.filter_experiences(@filter_quality, @filter_range)
 	end
 
